4243049a7fadf5ca8903c65e6def2edba0530faa
hiro <hmaeyama0510@gmail.com>
2021-05-30 08:15:31 +0900

    Add optional(?) on complete prop /set the default state /add complete prop on App.tsx

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,11 +1,11 @@
 type TodoType = {
   userId: number;
   title: string;
-  completed: boolean;
+  completed?: boolean;
 };
 
 export const Todo = (props: TodoType) => {
-  const { title, userId, completed } = props;
+  const { title, userId, completed = false } = props;
   const completeMMark = completed ? "D" : "ND";
   return <p>{`${completeMMark} ${title}(User ID:${userId})`}</p>;
 };
