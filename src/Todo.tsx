9147a6f432ddffaada8dc6fceca9863a79de1e04
hiro <hmaeyama0510@gmail.com>
2021-05-30 08:32:21 +0900

    Tried Omit

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,6 @@
 import { TodoType } from "./types/todo";
 
-export const Todo = (
-  props: Pick<TodoType, "userId" | "title" | "completed">
-) => {
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMMark = completed ? "D" : "ND";
   return <p>{`${completeMMark} ${title}(User ID:${userId})`}</p>;
